3904a0cf1c1bb0430e87f0dfdabbbeec8a8f6bb1
maheshwari12344 <alliswell369441@gmail.com>
2023-05-19 23:26:21 +0530

    game begins

---
+++ TicTacToeApp.java
@@ -0,0 +1,12 @@
+package tictactoe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicTacToeApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicTacToeApp.class, args);
+    }
+}
